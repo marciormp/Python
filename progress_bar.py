32629ef6e685f8ecba8eebe5648d832d9c9bcf27
Marcio Pereira <marciormp@live.com>
2024-04-01 16:50:08 -0300

    progress bar - updated

--- progress_bar.py
+++ progress_bar.py
@@ -9,7 +9,7 @@ sys.stdout.flush()
 sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
 
 for i in range(toolbar_width):
-    time.sleep(0.1) # do real work here
+    time.sleep(0.01) # do real work here
     # update the bar
     sys.stdout.write("|")
     sys.stdout.flush()
